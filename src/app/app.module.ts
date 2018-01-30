0781a8ec000482b5ff4ac60ede2b64e1aa003283
Jan <verstegenheijboer@gmail.com>
2018-01-30 10:27:06 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PersonenComponent } from './personen/personen.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonenComponent
   ],
   imports: [
     BrowserModule
